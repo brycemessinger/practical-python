a1554990a998da7821dacc206fa77786d3c443b7
bryce messinger <brycemessinger48@gmail.com>
2022-01-11 16:21:21 -0700

    first iteration

---
+++ tax-calculator.py
@@ -0,0 +1,5 @@
+my_name = input("What's your name?\n")
+hello = "Hello"
+
+greeting = hello + " " + my_name
+print(greeting)
---
+++ weather.py
@@ -0,0 +1,31 @@
+temperature = 70
+forecast = "rain"
+
+if not forecast == "rain":
+     print("Go outside!")
+else:
+    print("Stay inside!")
+
+# if temperature < 80 and forecast != "rain":
+#     print("Go Outside!")
+# else:
+#     print("Stay Inside!")
+
+# if temperature > 80 or temperature < 60:
+#     print("Stay inside!")
+# else:
+#     print("Enjoy the outdoors!")
+
+
+# if temperature > 80:
+#     print("It's too hot!")
+#     print("Stay inside!")
+# elif temperature < 60:
+#     print("It's too cold!")
+#     print("Stay inside!")
+# else:
+#     print("Enjoy the outdoors")
+
+
+
+print("Have a good day!")
